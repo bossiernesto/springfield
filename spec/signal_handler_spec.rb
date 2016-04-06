c86bc4a81c63a824fa03ca8e27878b28303664f5
Ernesto Bossi <bossi.ernestog@gmail.com>
2016-04-06 13:38:06 -0300

    Fix broken tests

--- spec/signal_handler_spec.rb
+++ spec/signal_handler_spec.rb
@@ -8,30 +8,26 @@ describe 'Signal Handler testing' do
     # Use the system default TERM handler instead, which does kill RSpec.
     # If you test a different signal you might not need to do this,
     # or you might need to install a different signal's handler.
-    old_signal_handler = Signal.trap 'TERM', 'SYSTEM_DEFAULT'
-
-    Reactor::SignalHandler.define_trap(:TERM) do
-      puts 'Exiting'
-      exit
+    pid = fork do
+      Reactor::SignalHandler.define_trap(:TERM) do
+        exit
+      end
+      expect(Reactor::SignalHandler).to receive(:define_trap).with array_including(:TERM)
     end
-    expect(Reactor::SignalHandler).to receive(:define_trap).with array_including(:TERM)
-    Process.kill 'TERM', 0 # Send the signal to ourself
 
-    # Put the Ruby default signal handler back in case it matters to other tests
-    Signal.trap 'TERM', old_signal_handler
+    Process.detach(pid)
+    Process.kill :TERM, pid # Send the signal to ourself
   end
 
   it 'test term_trap exit signal' do
-    old_signal_handler = Signal.trap 'TERM', 'SYSTEM_DEFAULT'
-
-    Reactor::SignalHandler.define_term_trap do
-      puts 'Exiting'
-      exit
+    pid = fork do
+      Reactor::SignalHandler.define_term_trap do
+        exit
+      end
+      expect(Reactor::SignalHandler).to receive(:define_term_trap)
     end
-    expect(Reactor::SignalHandler).to receive(:define_term_trap)
-    Process.kill 'TERM', 0 # Send the signal to ourself
 
-    # Put the Ruby default signal handler back in case it matters to other tests
-    Signal.trap 'TERM', old_signal_handler
+    Process.detach(pid)
+    Process.kill :TERM, pid # Send the signal to ourself
   end
 end
